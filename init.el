5a081de18129dd5033f99ab075abc2ecf5c7650c
Winfred Lu <winfred.lu@gmail.com>
2011-12-21 14:18:03 +0800

    1. Add .emacs.d and vendor to load-path 2. Add winfred-color-theme 3. Comment others temporarily

--- init.el
+++ init.el
@@ -1,11 +1,18 @@
-(setq load-path (cons "/usr/share/emacs/site-lisp/emacs-goodies-el" load-path))
-(setq load-path (cons "~/.emacs.d/" load-path))
+;; Winfred's Emacs Enrionment
 
-;;; color terminal
-(add-to-list 'load-path "color-theme.el")
+(add-to-list 'load-path "~/.emacs.d")
+;Add all top-level subdirectories of .emacs.d to the load path
+(progn (cd "~/.emacs.d")
+       (normal-top-level-add-subdirs-to-load-path))
+;Keep third party libraries seperate in ~/.emacs.d/vendor
+(add-to-list 'load-path "~/.emacs.d/vendor")
+(progn (cd "~/.emacs.d/vendor")
+       (normal-top-level-add-subdirs-to-load-path))
+
+;;; color theme
 (require 'color-theme)
-(color-theme-initialize)
-(eval-after-load "color-theme" '(color-theme-ld-dark))
+(load-library "winfred-color-theme")
+(color-theme-winfred)
 
 ;;; viper
 ;(setq viper-mode t)
@@ -28,69 +35,69 @@
 ;(define-key viper-insert-basic-map (kbd "j") 'viper-escape-if-next-char-is-j)
 
 ;;; Evil
-(add-to-list 'load-path "~/.emacs.d/evil")
-(require 'evil)
-(evil-mode 1)
+;(add-to-list 'load-path "~/.emacs.d/evil")
+;(require 'evil)
+;(evil-mode 1)
 
-(define-key evil-insert-state-map "j" #'cofi/maybe-exit)
-(evil-define-command cofi/maybe-exit ()
-  :repeat change
-  (interactive)
-  (let ((modified (buffer-modified-p)))
-    (insert "j")
-    (let ((evt (read-event (format "Insert %c to exit insert state" ?j)
-               nil 0.5)))
-      (cond
-       ((null evt) (message ""))
-       ((and (integerp evt) (char-equal evt ?j))
-    (delete-char -1)
-    (set-buffer-modified-p modified)
-    (push 'escape unread-command-events))
-       (t (setq unread-command-events (append unread-command-events
-                          (list evt))))))))
+;(define-key evil-insert-state-map "j" #'cofi/maybe-exit)
+;(evil-define-command cofi/maybe-exit ()
+  ;:repeat change
+  ;(interactive)
+  ;(let ((modified (buffer-modified-p)))
+    ;(insert "j")
+    ;(let ((evt (read-event (format "Insert %c to exit insert state" ?j)
+               ;nil 0.5)))
+      ;(cond
+       ;((null evt) (message ""))
+       ;((and (integerp evt) (char-equal evt ?j))
+    ;(delete-char -1)
+    ;(set-buffer-modified-p modified)
+    ;(push 'escape unread-command-events))
+       ;(t (setq unread-command-events (append unread-command-events
+                          ;(list evt))))))))
 
 ; make evil work for org-mode!
-(define-key evil-normal-state-map "O" (lambda ()
-                     (interactive)
-                     (end-of-line)
-                     (org-insert-heading)
-                     (evil-append nil)
-                     ))
+;(define-key evil-normal-state-map "O" (lambda ()
+                     ;(interactive)
+                     ;(end-of-line)
+                     ;(org-insert-heading)
+                     ;(evil-append nil)
+                     ;))
 
-(define-key evil-normal-state-map "[[" 'c-beginning-of-defun)
-(define-key evil-normal-state-map "[]" 'c-end-of-defun)
+;(define-key evil-normal-state-map "[[" 'c-beginning-of-defun)
+;(define-key evil-normal-state-map "[]" 'c-end-of-defun)
 
 ;;; org mod settings
-(require 'org-install)
-(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
-(setq org-agenda-include-diary t)
-(setq org-agenda-files (list "~/org/work.org"
-                             "~/org/home.org"))
-(setq browse-url-browser-function (quote browse-url-generic))
-(setq browse-url-generic-program "chromium-browser")
+;(require 'org-install)
+;(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+;(define-key global-map "\C-cl" 'org-store-link)
+;(define-key global-map "\C-ca" 'org-agenda)
+;(setq org-log-done t)
+;(setq org-agenda-include-diary t)
+;(setq org-agenda-files (list "~/org/work.org"
+                             ;"~/org/home.org"))
+;(setq browse-url-browser-function (quote browse-url-generic))
+;(setq browse-url-generic-program "chromium-browser")
 
 ;; mobile org settings
-(setq org-directory "~/org")
-(setq org-mobile-inbox-for-pull "~/org/flagged.org")
-(setq org-mobile-directory "~/Dropbox/org")
+;(setq org-directory "~/org")
+;(setq org-mobile-inbox-for-pull "~/org/flagged.org")
+;(setq org-mobile-directory "~/Dropbox/org")
 
 ;;; buffer switching
-(global-set-key (kbd "C-x C-b") 'buffer-menu)
-(iswitchb-mode 1)
-(setq iswitchb-buffer-ignore '("^ " "*Buffer"))
-(setq iswitchb-delim " | ")
+;(global-set-key (kbd "C-x C-b") 'buffer-menu)
+;(iswitchb-mode 1)
+;(setq iswitchb-buffer-ignore '("^ " "*Buffer"))
+;(setq iswitchb-delim " | ")
 
 ;;; c settings
-(require 'cc-mode)
-(setq c-default-style "linux"
-      c-basic-offset 8)
-(define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
+;(require 'cc-mode)
+;(setq c-default-style "linux"
+      ;c-basic-offset 8)
+;(define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
 
 ;;; magit
-(require 'magit)
+;(require 'magit)
 
 ;;; auto pair
 (require 'autopair)
