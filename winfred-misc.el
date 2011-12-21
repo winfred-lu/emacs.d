41ab929538bbe0c29c04688326a1480571ba3719
Winfred Lu <winfred.lu@gmail.com>
2011-12-21 17:31:44 +0800

    1. Remove autopair (use YASnippet instead) 2. y-or-n instead of yes-or-no

--- winfred-misc.el
+++ winfred-misc.el
@@ -1,6 +1,5 @@
-;; auto pair
-(require 'autopair)
-(autopair-global-mode)
-
 ;; Remove unnecessary trailing whitespace
 (add-hook 'before-save-hook (lambda () (delete-trailing-whitespace)))
+
+;; y-or-n
+(defalias 'yes-or-no-p 'y-or-n-p)
