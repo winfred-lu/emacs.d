3c5e8dfe6120b75d8d26bb1e77be6e6d4e1e9b63
Winfred Lu <winfred.lu@gmail.com>
2011-12-31 21:45:14 +0800

    Fix for unable to compile at different directory.

--- vendor/compile.sh
+++ vendor/compile.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-(cd evil && make)
-(cd magit && make)
+dir=`dirname $0`
+
+(cd $dir/evil && make)
+(cd $dir/magit && make)
 
 if type rake > /dev/null 2>&1; then
-	(cd yasnippet && rake compile)
+	(cd $dir/yasnippet && rake compile)
 fi
 
-emacs --batch -Q -f batch-byte-compile autopair.el
-emacs --batch -Q -f batch-byte-compile color-theme.el
-emacs --batch -Q -f batch-byte-compile smart-tab/smart-tab.el
+emacs --batch -Q -f batch-byte-compile $dir/autopair.el
+emacs --batch -Q -f batch-byte-compile $dir/color-theme.el
+emacs --batch -Q -f batch-byte-compile $dir/smart-tab/smart-tab.el
 
