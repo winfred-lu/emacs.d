89464d4e413bcfda37023461cb58e61a193598c7
Winfred Lu <winfred.lu@gmail.com>
2012-01-02 15:41:41 +0800

    Able to copy and paste between emacs and other apps

--- winfred-misc.el
+++ winfred-misc.el
@@ -11,3 +11,7 @@
 ;; chromium as default browser
 (setq browse-url-browser-function 'browse-url-generic
       browse-url-generic-program "chromium-browser")
+
+;; to copy and paste between emacs and other apps
+(setq x-select-enable-clipboard t)
+(setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
