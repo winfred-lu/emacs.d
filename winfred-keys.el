2247f87f006c63d88afed7e2cc10fcbe373ced16
Winfred Lu <winfred.lu@gmail.com>
2012-01-05 15:43:05 +0800

    Undo evil's C-e remapping.

--- winfred-keys.el
+++ winfred-keys.el
@@ -1,4 +1,4 @@
-;;;; global key bindings ;;;;
+;;;;;; global key bindings (modeless) ;;;;;;
 
 ;; F11 to toggle whitespace-mode on/off
 (global-set-key [(f11)] 'whitespace-mode)
@@ -6,13 +6,16 @@
 ;; auto indentation after RET
 ;(global-set-key (kbd "RET") 'newline-and-indent)
 
+;; undo the evil's remapping C-e to evil-scroll-line-down
+(define-key evil-insert-state-map "\C-e" 'end-of-line)
+
 ;; functions frequently used
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-xg" 'magit-status)
 
-;;;; local key bindings ;;;;
+;;;;;; local key bindings (modeless) ;;;;;;
 
 ;; key bindings for html-mode
 (add-hook 'html-mode-hook
 	  (lambda ()
-	  (local-set-key (kbd "C-c o") 'browse-url-at-point)))
+	    (local-set-key (kbd "C-c o") 'browse-url-at-point)))
