123119a8fc61754dff310fcfdeddb2e25ed83b9a
Winfred Lu <winfred.lu@gmail.com>
2012-02-29 21:39:01 +0800

    Use ido instead of iswitchb.

--- winfred-buffer.el
+++ winfred-buffer.el
@@ -1,7 +1,7 @@
 ;; backups and autosaves
-(setq backup-directory-alist `((".*" . "~/.emacs.d/backups")))
-(setq auto-save-file-name-transforms `((".*" "~/.emacs.d/autosaves/\\1" t)))
-(setq require-final-newline t)
+(setq backup-directory-alist `((".*" . "~/.emacs.d/backups"))
+      auto-save-file-name-transforms `((".*" "~/.emacs.d/autosaves/\\1" t))
+      require-final-newline t)
 
 ;;;;;;
 
@@ -16,30 +16,37 @@
 
 ;;;;;;
 
+(ido-mode t)
+(setq ido-enable-flex-matching t)
+(add-to-list 'ido-ignore-buffers "\\*Customize")
+(add-to-list 'ido-ignore-buffers "\\*Buffer")
+
 ;; mini buffer switching
-(iswitchb-mode 1)
+;(iswitchb-mode 1)
 
 ;; '|' as the delimiter
-(setq iswitchb-delim " | ")
+;(setq iswitchb-delim " | ")
 
-;; Ignore all non-file buffers excluding those listed in `exc-files'
-(defun iswitchb-ignore-non-file (name)
-  (let ((exc-files '("*info*" "*scratch*")))
-    (and (not (member name exc-files))
-         (eq 42 (aref name 0))
-         (eq 42 (aref name (- (length name) 1))))))
-(setq iswitchb-buffer-ignore '("^ " iswitchb-ignore-non-file))
+;; Ignore all non-file buffers excluding those listed in `inc-buffers'
+;(defun iswitchb-ignore-non-file (name)
+;  (let ((inc-buffers '("*info*" "*scratch*"))
+;        (exc-files '("TAGS")))
+;    (or (member name exc-files)
+;        (and (not (member name inc-buffers))
+;             (eq 42 (aref name 0))
+;             (eq 42 (aref name (- (length name) 1)))))))
+;(setq iswitchb-buffer-ignore '("^ " iswitchb-ignore-non-file))
 
 ;; Keep the size of iswitchb minibuffer more manageable.
-(add-hook 'iswitchb-minibuffer-setup-hook
-          '(lambda () (set (make-local-variable 'max-mini-window-height) 3)))
+;(add-hook 'iswitchb-minibuffer-setup-hook
+;          '(lambda () (set (make-local-variable 'max-mini-window-height) 3)))
 
-(defun iswitchb-exclude-nonmatching()
-  "Make iswitchb work on only the currently matching names."
-  (interactive)
-  (setq iswitchb-buflist iswitchb-matches)
-  (setq iswitchb-rescan t)
-  (delete-minibuffer-contents))
+;(defun iswitchb-exclude-nonmatching()
+;  "Make iswitchb work on only the currently matching names."
+;  (interactive)
+;  (setq iswitchb-buflist iswitchb-matches)
+;  (setq iswitchb-rescan t)
+;  (delete-minibuffer-contents))
 
 ;;;;;;
 
