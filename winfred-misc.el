62f71ca0a3c4087bce294dddced1050373c70abd
Winfred Lu <winfred.lu@gmail.com>
2012-03-08 22:36:53 +0800

    Add eval-and-replace.

--- winfred-misc.el
+++ winfred-misc.el
@@ -21,8 +21,22 @@
       browse-url-generic-program "chromium-browser")
 
 ;; to copy and paste between emacs and other apps
-(setq x-select-enable-clipboard t)
-(setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
+(setq x-select-enable-clipboard t
+      interprogram-paste-function 'x-cut-buffer-or-selection-value)
 
 ;; stop y-and-n before editing symlinks
 (setq vc-follow-symlinks nil)
+
+
+;; http://irreal.org/blog/?p=297  http://nflath.com/2009/08/easier-emacs/
+(defun eval-and-replace ()
+  "Replace the preceding sexp with its value."
+  (interactive)
+  (backward-kill-sexp)
+  (condition-case nil
+      (prin1 (eval (read (current-kill 0)))
+             (current-buffer))
+    (error (message "Invalid expression")
+           (insert (current-kill 0)))))
+(global-set-key "\C-ce" 'eval-and-replace)
+
