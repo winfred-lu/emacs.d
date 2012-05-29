0514cd800a1902e155b2e533c24c4ec8d6ebedd2
Winfred Lu <winfred.lu@gmail.com>
2012-05-29 21:50:21 +0800

    Enable dired-x mode when entering dired. Ido ignores the buffers without an associated file.

--- winfred-buffer.el
+++ winfred-buffer.el
@@ -1,6 +1,7 @@
 ;; backups and autosaves
 (setq backup-directory-alist `((".*" . "~/.d.emacs.d/backups"))
       auto-save-file-name-transforms `((".*" "~/.d.emacs.d/autosaves/\\1" t))
+      auto-save-list-file-prefix "~/.d.emacs.d/auto-save-list/.saves-"
       require-final-newline t)
 
 ;; bookmarks file and autosaving
@@ -11,32 +12,33 @@
 (require 'uniquify)
 (setq uniquify-buffer-name-style (quote forward))
 
+(require 'browse-kill-ring)
+
 ;;;;;;
 
 (autoload 'dired-jump "dired" t nil)
-
+(add-hook 'dired-load-hook
+          (lambda ()
+            (load "dired-x")
+            ))
+(add-hook 'dired-mode-hook
+          (lambda ()
+            (dired-omit-mode 1)
+            ))
 ;;;;;;
 
 (setq ido-enable-flex-matching t
-      ido-everywhere t)
+      ido-everywhere t
+      ido-max-directory-size 100000)
 (ido-mode t)
-(loop for b in '("\\*Backtrace" "\\*Bookmark"
-		 "\\*Buffer" "\\*Completions" "\\*Customize" "\\*Help"
-                 "\\*Ibuffer"
-                 "\\*Messages" "\\*cscope" "\\*etags-select" "\\*grep"
-                 "\\*magit" "\\*scratch")
-      do (add-to-list 'ido-ignore-buffers b))
+(put 'dired-do-rename 'ido nil)
+(defun wf-ido-ignore-buf (name)
+  (with-current-buffer name (not buffer-file-name)))
+(setq ido-ignore-buffers '("^ " wf-ido-ignore-buf))
 (loop for d in '("\\`auto" "\\`backups/" "\\`CVS/")
       do (add-to-list 'ido-ignore-directories d))
-(loop for f in '("tags" "\\`cscope." "\\.dep$")
+(loop for f in '("tags" "\\`cscope." "\\.dep$" ".d.emacs.d" ".dropbox")
       do (add-to-list 'ido-ignore-files f))
-
-;;;;;;
-
-(defun show-buffer-full-name ()
-  "Show the full path file name in the minibuffer."
-  (interactive)
-  (message (buffer-file-name)))
 
 ;;;;;;
 
