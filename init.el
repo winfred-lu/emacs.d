2cc608b3958ad00ed1b11e3d472a7240604d3128
Winfred Lu <winfred.lu@gmail.com>
2012-06-07 22:37:21 +0800

    Rename cc-mode to program.

--- init.el
+++ init.el
@@ -8,21 +8,22 @@
   (normal-top-level-add-subdirs-to-load-path))
 
 ;; Use ELPA (emacs 24.x)
-(if (>= emacs-major-version 24)
-    (progn (require 'package)
-	   (package-initialize)
-	   (setq package-archives
-		 '(("ELPA" . "http://tromey.com/elpa/")
-		   ("gnu" . "http://elpa.gnu.org/packages/")
-		   ("marmalade" . "http://marmalade-repo.org/packages/")))))
+;; (if (>= emacs-major-version 24)
+;;     (progn (require 'package)
+;; 	   (package-initialize)
+;; 	   (setq package-archives
+;; 		 '(("ELPA" . "http://tromey.com/elpa/")
+;; 		   ("gnu" . "http://elpa.gnu.org/packages/")
+;; 		   ("marmalade" . "http://marmalade-repo.org/packages/")))))
 
 (load-library "winfred-visual")
 (load-library "winfred-evil")
 (load-library "winfred-buffer")
 (load-library "winfred-misc")
 (load-library "winfred-keys")
-(load-library "winfred-cc-mode")
+(load-library "winfred-program")
 (load-library "winfred-org")
 (require 'etags-select)
 (require 'magit)
 (require 'xcscope)
+
