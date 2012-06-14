6a150099b7def9530635794fb49a511ccc837dee
Winfred Lu <winfred.lu@gmail.com>
2012-06-14 22:22:17 +0800

    Add functions: major-mode, what-face.

--- winfred-misc.el
+++ winfred-misc.el
@@ -33,7 +33,13 @@
 (defun switch-to-liu ()
   (interactive)
   (activate-input-method "chinese-liu5"))
-(global-set-key (kbd "<C-f9>") 'switch-to-liu)
+(global-set-key (kbd "<C-f8>") 'switch-to-liu)
+
+;; prints the major mode of current buffer
+(defun major-mode (buf)
+  (interactive "b")
+  (with-current-buffer buf
+    (message "major-mode of %s is %s" buf major-mode)))
 
 ;; http://irreal.org/blog/?p=297
 ;; http://nflath.com/2009/08/easier-emacs/
@@ -47,4 +53,11 @@
     (error (message "Invalid expression")
            (insert (current-kill 0)))))
 (global-set-key "\C-ce" 'eval-and-replace)
+
+;; http://stackoverflow.com/questions/1242352/get-font-face-under-cursor-in-emacs
+(defun what-face (pos)
+  (interactive "d")
+  (let ((face (or (get-char-property (point) 'read-face-name)
+                  (get-char-property (point) 'face))))
+    (if face (message "Face: %s" face) (message "No face at %d" pos))))
 
