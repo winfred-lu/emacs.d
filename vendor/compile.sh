154533e6d6fcc33d08291052e06090cf16edc512
Winfred Lu <winfred.lu@gmail.com>
2013-01-20 18:07:28 +0800

    Add idomenu.el

--- vendor/compile.sh
+++ vendor/compile.sh
@@ -11,10 +11,11 @@ fi
 
 emacs --batch -Q -f batch-byte-compile $dir/ace-jump-mode.el
 emacs --batch -Q -f batch-byte-compile $dir/autopair.el
+emacs --batch -Q -f batch-byte-compile $dir/browse-kill-ring.el
 emacs --batch -Q -f batch-byte-compile $dir/color-theme.el
 emacs --batch -Q -f batch-byte-compile $dir/csharp-mode-0.8.5.el
 emacs --batch -Q -f batch-byte-compile $dir/hide-region.el
-emacs --batch -Q -f batch-byte-compile $dir/browse-kill-ring.el
+emacs --batch -Q -f batch-byte-compile $dir/idomenu.el
 emacs --batch -Q -f batch-byte-compile $dir/smart-tab/smart-tab.el
 emacs --batch -Q -f batch-byte-compile $dir/xcscope.el
 
