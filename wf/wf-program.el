516f1078039e8b027f7712b4dfd56d7cf9f22bfe
Winfred Lu <winfred.lu@gmail.com>
2016-08-19 20:06:11 +0800

    Remove loading rope

--- wf/wf-program.el
+++ wf/wf-program.el
@@ -107,5 +107,5 @@
 (autoload 'pymacs-eval "pymacs" nil t)
 (autoload 'pymacs-exec "pymacs" nil t)
 (autoload 'pymacs-load "pymacs" nil t)
-(pymacs-load "ropemacs" "rope-")
-(setq ropemacs-enable-autoimport t)
+;(pymacs-load "ropemacs" "rope-")
+;(setq ropemacs-enable-autoimport t)
