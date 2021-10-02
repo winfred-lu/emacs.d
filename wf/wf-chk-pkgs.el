b75d94d8a12ced61262fe4e67373286b4dfad32d
Winfred Lu <winfred.lu@gmail.com>
2021-10-02 17:49:43 +0800

    Use yasnippet-snippets package

--- wf/wf-chk-pkgs.el
+++ wf/wf-chk-pkgs.el
@@ -15,6 +15,7 @@
     undo-tree           ; Evil requires undo-tree.el for linear undo and undo branches
     xcscope
     yasnippet
+    yasnippet-snippets
     zenburn-theme
     ) "a list of packages to ensure are installed")
 
