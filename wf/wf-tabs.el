a908e905be118d16bf771e8e8b784dfab903c312
Winfred Lu <winfred.lu@gmail.com>
2021-11-02 23:23:35 +0800

    Delete unused

--- wf/wf-tabs.el
+++
@@ -1,8 +0,0 @@
-;; smart-tab
-(use-package smart-tab-mode
-  :init
-  (add-to-list 'hippie-expand-try-functions-list
-               'yas/hippie-try-expand) ;put yasnippet in hippie-expansion list
-  (setq smart-tab-using-hippie-expand t)
-  :config
-  (global-smart-tab-mode 1))
--- wf/wf-yasnippet.el
+++
@@ -1,6 +0,0 @@
-;; yasnippet settings
-(use-package yasnippet
-  :hook ((c-mode . yas-minor-mode)
-         (python-mode . yas-minor-mode)
-         (sh-mode . yas-minor-mode)
-         (emacs-lisp-mode . yas-minor-mode)))
